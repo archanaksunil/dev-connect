b90bbd6f7b3620efedfedd7749b5be4b3b6913a2
archanaksunil <archana.k.sunil@gmail.com>
2025-01-12 21:50:23 -0700

    add validator

--- src/app.js
+++ src/app.js
@@ -17,77 +17,82 @@ app.post("/signup", async (req, res) => {
 });
 
 app.get("/user", async (req, res) => {
-    const userEmail = req.body.email;
-    try {
-        const users = await User.findOne({email: userEmail});
-        // const users = await User.find({email: userEmail});
-        if(!users) {
-            res.status(404).send("User not found");
-        } else {
-            res.send(users);
-        }
-    } catch(err) {
-        res.status(400).send("Error");
+  const userEmail = req.body.email;
+  try {
+    const users = await User.findOne({ email: userEmail });
+    // const users = await User.find({email: userEmail});
+    if (!users) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(users);
     }
+  } catch (err) {
+    res.status(400).send("Error");
+  }
 });
 
 app.get("/feed", async (req, res) => {
-    try {
-        const users = await User.find({});
-        if(users.length === 0) {
-            res.status(404).send("No user");
-        } else {
-            res.send(users);
-        }
-    } catch(err) {
-        res.status(400).send("Error");
+  try {
+    const users = await User.find({});
+    if (users.length === 0) {
+      res.status(404).send("No user");
+    } else {
+      res.send(users);
     }
+  } catch (err) {
+    res.status(400).send("Error");
+  }
 });
 
 app.get("/userById", async (req, res) => {
-    const id = req.body._id;
-    try {
-        const users = await User.findById(id);
-        if(!users) {
-            res.status(404).send("User not found");
-        } else {
-            res.send(users);
-        }
-    } catch(err) {
-        res.status(400).send("Error");
+  const id = req.body._id;
+  try {
+    const users = await User.findById(id);
+    if (!users) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(users);
     }
+  } catch (err) {
+    res.status(400).send("Error");
+  }
 });
 
 app.delete("/user", async (req, res) => {
-    const id = req.body.userId;
-    try {
-        await User.findByIdAndDelete(id);
-        res.send("User deleted")
-    } catch(err) {
-        res.status(400).send("Error");
-    }
+  const id = req.body.userId;
+  try {
+    await User.findByIdAndDelete(id);
+    res.send("User deleted");
+  } catch (err) {
+    res.status(400).send("Error");
+  }
 });
 
 app.patch("/userByID", async (req, res) => {
-    const id = req.body._id;
-    const body = req.body;
-    try {
-        await User.findByIdAndUpdate(id, body, {returnDocument: "before"});
-        res.send("updated successfully");
-    } catch(err) {
-        res.status(400).send("Error");
-    }
+  const id = req.body._id;
+  const body = req.body;
+  try {
+    await User.findByIdAndUpdate(id, body, {
+      returnDocument: "before",
+      runValidators: true,
+    });
+    res.send("updated successfully");
+  } catch (err) {
+    res.status(400).send("Error");
+  }
 });
 
 app.patch("/userByEmail", async (req, res) => {
-    const userEmail = req.body.email;
-    const body = req.body;
-    try {
-        await User.findOneAndUpdate({email: userEmail}, body);
-        res.send("updated successfully");
-    } catch(err) {
-        res.status(400).send("Error");
-    }
+  const userEmail = req.body.email;
+  const body = req.body;
+  try {
+    await User.findOneAndUpdate({ email: userEmail }, body, {
+      runValidators: true,
+    });
+    res.send("updated successfully");
+  } catch (err) {
+    res.status(400).send("Error "+err);
+  }
 });
 
 connectDb()
--- src/models/user.js
+++ src/models/user.js
@@ -2,23 +2,50 @@ const moongose = require("mongoose");
 
 const userSchema = new moongose.Schema({
     firstName: {
-        type: String
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 3,
+        maxLength: 20
+
     },
     lastName: {
-        type: String
+        type: String,
     },
     email: {
-        type: String
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        minLength: 7,
+        maxLength: 50
     },
     password: {
-        type: String
+        type: String,
+        required: true
     },
     age: {
-        type: Number
+        type: Number,
+        trim: true,
+        min: 10
     },
     gender: {
-        type: String
+        type: String,
+        validate(value) {
+            if(!["male", "female", "other"].includes(value)) throw new Error("Gender not valid");
+        }
+    },
+    skills: {
+        type: [String]
+    },
+    photoUrl: {
+        type: String,
+        default: "https://avatar.iran.liara.run/public/boy?username=Ash"
     }
+}, 
+{
+    timestamps: true
 });
 
 module.exports = moongose.model("User", userSchema);
