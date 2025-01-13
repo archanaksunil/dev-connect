30ca4281f7b0d050a6c54aa97b5384044c0b3726
archanaksunil <archana.k.sunil@gmail.com>
2025-01-12 20:08:20 -0700

    add api

--- src/app.js
+++ src/app.js
@@ -4,20 +4,90 @@ const User = require("./models/user");
 
 const app = express();
 
-app.post("/signup", (req, res) => {
-  const newUser = new User({
-    firstName: "lady",
-    lastName: "kiki",
-    email: "angelLady@gmail.com",
-    age: 3,
-    gender: "female",
-  });
+app.use(express.json());
+
+app.post("/signup", async (req, res) => {
+  const user = new User(req.body);
   try {
-    newUser.save();
+    await user.save();
     res.send("User saved");
   } catch (err) {
     res.status(400).send(err.message);
   }
+});
+
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.email;
+    try {
+        const users = await User.findOne({email: userEmail});
+        // const users = await User.find({email: userEmail});
+        if(!users) {
+            res.status(404).send("User not found");
+        } else {
+            res.send(users);
+        }
+    } catch(err) {
+        res.status(400).send("Error");
+    }
+});
+
+app.get("/feed", async (req, res) => {
+    try {
+        const users = await User.find({});
+        if(users.length === 0) {
+            res.status(404).send("No user");
+        } else {
+            res.send(users);
+        }
+    } catch(err) {
+        res.status(400).send("Error");
+    }
+});
+
+app.get("/userById", async (req, res) => {
+    const id = req.body._id;
+    try {
+        const users = await User.findById(id);
+        if(!users) {
+            res.status(404).send("User not found");
+        } else {
+            res.send(users);
+        }
+    } catch(err) {
+        res.status(400).send("Error");
+    }
+});
+
+app.delete("/user", async (req, res) => {
+    const id = req.body.userId;
+    try {
+        await User.findByIdAndDelete(id);
+        res.send("User deleted")
+    } catch(err) {
+        res.status(400).send("Error");
+    }
+});
+
+app.patch("/userByID", async (req, res) => {
+    const id = req.body._id;
+    const body = req.body;
+    try {
+        await User.findByIdAndUpdate(id, body, {returnDocument: "before"});
+        res.send("updated successfully");
+    } catch(err) {
+        res.status(400).send("Error");
+    }
+});
+
+app.patch("/userByEmail", async (req, res) => {
+    const userEmail = req.body.email;
+    const body = req.body;
+    try {
+        await User.findOneAndUpdate({email: userEmail}, body);
+        res.send("updated successfully");
+    } catch(err) {
+        res.status(400).send("Error");
+    }
 });
 
 connectDb()
